47f51242eddeebaceac54c3109a9c39181aa5b2c
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-10-09 11:17:06 +0600

    update dot net core file

--- My_Ecommerce/My_Ecommerce/Controllers/HomeController.cs
+++ My_Ecommerce/My_Ecommerce/Controllers/HomeController.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using My_Ecommerce.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace My_Ecommerce.Controllers
 {
