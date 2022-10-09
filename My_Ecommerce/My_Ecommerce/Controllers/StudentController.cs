f27658d204a244c2dcdcdc7b4fac95151256b678
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-10-09 13:59:39 +0600

    add student controller

---
+++ My_Ecommerce/My_Ecommerce/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My_Ecommerce.Controllers
+{
+    public class StudentController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index ()
+        {
+            return "Default Action";
+        } 
+
+        public string Welcome()
+        {
+            return "Welcome Actoon";
+        }
+    }
+}
